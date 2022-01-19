ddb7d0270aa85eea5604272a3df0b80d1d5a7357
serref jaguraga <reef163@gmail.com>
2022-01-19 13:02:24 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const array = [];
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+    array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+
+    return array;
+}
+
+function countDown (int) {
+    let countDown = 0;
+    while (countDown < 11) {
+        console.log(countDown++);
+    }
+}
