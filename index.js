5c57c2ea150fddef2cef6a1a2f390c2fd75e0581
serref jaguraga <reef163@gmail.com>
2022-02-02 17:40:15 -0500

    commit tree 631c8c358b6fd5bbe77319ecbe0c50d2ca8d8785

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const array = [];
+/*const array = [];
 function writeCards(name, event) {
     for (let i = 0; i < name.length; i++) {
     array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
@@ -9,8 +9,29 @@ function writeCards(name, event) {
 }
 
 function countDown (int) {
-    let countDown = 0;
-    while (countDown < 11) {
-        console.log(countDown++);
+    let i = 11;
+    while (i > 0) {
+        console.log(--i);
     }
+}*/
+
+
+
+const array = [];
+
+function writeCards(name, event) {
+for(let i = 0; i < name.length; i++) {
+array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+debugger;
 }
+return array;
+}
+
+
+function countDown (int) {
+    let i = 11;
+    while (i > 0) {
+        console.log(--i);
+      }
+}
+
